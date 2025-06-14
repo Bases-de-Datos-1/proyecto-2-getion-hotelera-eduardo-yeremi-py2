6d2b068f9d35c893937715bca93f61399ac86b8e
YeremiCalvoPorras <125587678+Yeremi889@users.noreply.github.com>
2025-06-13 18:27:59 -0600

    login
    termine login y empece menu aunque me falta interactividad en el

---
+++ codigo/GestionHotelera/Controllers/ClienteController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GestionHotelera.Controllers
+{
+    public class ClienteController : Controller
+    {
+        // GET: /Cliente/Menu
+        [HttpGet]
+        public IActionResult Menu()
+        {
+            return View();
+        }
+    }
+}
--- codigo/GestionHotelera/Controllers/CuentaController.cs
+++ codigo/GestionHotelera/Controllers/CuentaController.cs
@@ -5,20 +5,18 @@ namespace GestionHotelera.Controllers
 {
     public class CuentaController : Controller
     {
-        // GET: /Cuenta/Login
+        // Cuenta/Login
         public IActionResult Login()
         {
             return View();
         }
 
-        // POST: /Cuenta/Login
         [HttpPost]
         public IActionResult Login(string email, string password)
         {
-            // Aquí iría la validación con la base de datos (usar sp_ValidarCliente)
-            if (email == "admin@example.com" && password == "123456") // Ejemplo temporal
+            if (email == "admin@example.com" && password == "123456")
             {
-                return RedirectToAction("Index", "Home"); // Redirigir al dashboard
+                return RedirectToAction("Index", "Home");
             }
             else
             {
@@ -27,18 +25,38 @@ namespace GestionHotelera.Controllers
             }
         }
 
-        // GET: /Cuenta/Register
+        // Cuenta/Register
         public IActionResult Register()
         {
             return View();
         }
 
-        // POST: /Cuenta/Register
         [HttpPost]
         public IActionResult Register(string email, string password)
         {
-            // Aquí iría el registro en la base de datos (usar sp_AgregarCliente)
-            return RedirectToAction("Login"); // Redirigir al login después del registro
+            return RedirectToAction("Login");
+        }
+
+        // Cuenta/RegisterCliente
+        [HttpGet]
+        public IActionResult RegisterCliente()
+        {
+            return View();
+        }
+
+        // Cuenta/RegisterHospedaje
+        [HttpGet]
+        public IActionResult RegisterHospedaje()
+        {
+            return View();
+        }
+
+
+        // Cuenta/RegisterRecreacion
+        [HttpGet]
+        public IActionResult RegisterRecreacion()
+        {
+            return View();
         }
     }
 }
