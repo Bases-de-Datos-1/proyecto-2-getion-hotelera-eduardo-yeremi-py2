93acb7eb0a940711551006afba086754b6b722f7
YeremiCalvoPorras <125587678+Yeremi889@users.noreply.github.com>
2025-06-15 23:55:16 -0600

    empresa recreacion terminada
    soy un saico legalmente, lo único que n hice fue lo de mostrar la información de las actividades xq para eso se ocupan ids y la información que se obtiene de la base de datos pero el resto es completamente dinámico y funciona

---
+++ codigo/GestionHotelera/Controllers/EmpresaRecreacionController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GestionHotelera.Controllers
+{
+    public class EmpresaRecreacionController : Controller
+    {
+        public IActionResult Menu()
+        {
+            return View();
+        }
+
+        public IActionResult NuevoServicio()
+        {
+            return View();
+        }
+
+        public IActionResult NuevaActividad()
+        {
+            return View();
+        }
+
+        public IActionResult EditarPerfil()
+        {
+            return View();
+        }
+
+        public IActionResult EliminarPerfil()
+        {
+            return View();
+        }
+
+        public IActionResult EditarActividad()
+        {
+            return View();
+        }
+    }
+}
